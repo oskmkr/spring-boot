f09367a7ed5a82dd63d2adcf8ddd3ab634a4fa24
oskm <oskmkr@naver.com>
2015-12-21 00:56:17 +0900

    doctc model 생성

---
+++ src/main/java/com/oskm/parser/DoctcEvent.java
@@ -0,0 +1,43 @@
+package com.oskm.parser;
+
+/**
+ * Created by oskm on 2015-12-21.
+ */
+public class DoctcEvent {
+    private String link;
+    private String mainImage;
+    private String title;
+    private String duration;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getMainImage() {
+        return mainImage;
+    }
+
+    public void setMainImage(String mainImage) {
+        this.mainImage = mainImage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
--- src/main/java/com/oskm/parser/html/HtmlPageParser.java
+++ src/main/java/com/oskm/parser/html/HtmlPageParser.java
@@ -1,16 +1,14 @@
 package com.oskm.parser.html;
 
+import com.oskm.parser.DoctcEvent;
 import com.oskm.parser.Parser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -26,19 +24,36 @@ public class HtmlPageParser implements Parser {
         Document doc = Jsoup.parse(new URL("http://www.doctc.com/shop/event/event_main.php"), 3000);
 
 
-        Elements el = doc.select("#event_top > div > div:nth-child(1)");
+        Elements elParent = doc.select("#event_top > div > div");
 
-        Elements thumbnail = el.select("div.thumbnail");
+        int size = elParent.size();
 
-        LOG.debug("상세 링크 : " + thumbnail.attr("onclick"));
-        LOG.debug("대표 이미지 : " + el.select("div.gdListImg > img").attr("src"));
-        LOG.debug("타이틀 : " + el.select("div.gdListText > div.gdListName").html());
-        LOG.debug("기간 : " + el.select("div.gdListText > div.gdDate").html());
+        LOG.debug("size : " + size);
+
+        for (int i = 0; i < size; i++) {
+            Elements el = doc.select("#event_top > div > div:nth-child(" + (i + 1) + ")");
+
+            Elements thumbnail = el.select("div.thumbnail");
+
+            DoctcEvent event = new DoctcEvent();
+
+            event.setLink(thumbnail.attr("onclick"));
+            event.setMainImage(el.select("div.gdListImg > img").attr("src"));
+            event.setTitle(el.select("div.gdListText > div.gdListName").html());
+            event.setDuration(el.select("div.gdListText > div.gdDate").html());
+
+            LOG.debug("상세 링크 : " + event.getLink());
+            LOG.debug("대표 이미지 : " + event.getMainImage());
+            LOG.debug("타이틀 : " + event.getTitle());
+            LOG.debug("기간 : " + event.getDuration());
+
+            //String s = el.html();
+
+            //LOG.debug(s);
+        }
 
 
-        String s = el.html();
-
-        LOG.debug(s);
+        //return event;
     }
 
 }
