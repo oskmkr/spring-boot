639b279f1b4b1dc7880700bc5e30aae495cbb98a
oskmkr <oskmkr@naver.com>
2016-03-05 03:03:26 +0900

    response body & POST request - added processing charset UTF-8

--- src/main/java/com/oskm/AppConfiguration.java
+++ src/main/java/com/oskm/AppConfiguration.java
@@ -1,24 +1,18 @@
 package com.oskm;
 
 import freemarker.template.TemplateException;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import javax.servlet.Filter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.charset.Charset;
 
 
 /**
@@ -44,6 +38,29 @@ public class AppConfiguration {
         result.setTemplateLoaderPaths("/"); // prevents FreeMarkerConfigurer from using its default path allowing setPrefix to work as expected
         return result;
     }
+
+    /**
+     * responseBodyConverter는 결과를 출력시에 강제로 UTF-8로 설정하는 역할
+     * @return
+     */
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    /**
+     * characterEncodingFilter는 POST 요청시에 한글이 깨지는 문제를 보완
+     * @return
+     */
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        return characterEncodingFilter;
+    }
+
     /*
     @Bean
     public ViewResolver getViewResolver() {
